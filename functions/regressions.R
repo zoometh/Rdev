2365e74be18aa2f1de5dc7b0aae95955d41537c1
zoometh <thomashuet7@gmail.com>
2021-09-24 20:18:32 +0200

    func

--- functions/regressions.R
+++ functions/regressions.R
@@ -2,8 +2,6 @@
 library(ggplot2) # pour les graphiques
 library(ggrepel) # pour écarter les labels
 
-getwd()
-
 # data: 2-columns
 depots <- read.table('https://raw.github.com/zoometh/Rdev/master/data/data_regression.csv',
                      header = T,
@@ -13,21 +11,26 @@ depots$id <- row.names(depots) # names
 typeA_B <- lm(depots$nb_typeA ~ depots$nb_typeB) # linear model
 
 # relation entre les variables à expliquer et des variables explicatives
-r.squared <- round(summary(typeA_B)$r.squared*100, 1)
-print(paste("la présence de '", colnames(depots)[1],
-            "' explique", r.squared, "% de la présence de '", colnames(depots)[2],
-            "' (et réciproquement)"))
+tit <- paste("la présence de '", colnames(depots)[1],
+            "' explique", round(summary(typeA_B)$r.squared*100, 1),
+            "% de \nla présence de '", colnames(depots)[2],
+            "' (coefficient de régression:", summary(typeA_B)$r.squared, ")")
 
 # afficher
-greg <- ggplot(depots, aes(x = nb_typeA, y = nb_typeB)) +
-  geom_smooth(method = "lm", se = FALSE) +
-  geom_point() +
-  geom_text_repel(aes(label = id)) +
+greg <- ggplot(depots, aes(color = color, shape = shape))+
+  geom_point(aes(nb_typeA, nb_typeB)) +
+  geom_text_repel(aes(nb_typeA, nb_typeB, label = id)) +
+  scale_color_identity() +
+  scale_shape_identity() +
   scale_x_continuous(breaks= c(min(depots$nb_typeA) : max(depots$nb_typeA))) +
-  scale_y_continuous(breaks= c(min(depots$nb_typeB) : max(depots$nb_typeB)))
+  scale_y_continuous(breaks= c(min(depots$nb_typeB) : max(depots$nb_typeB))) +
+  theme_bw()
 greg
+
 
 # sauver
 png("out/regression_depots.png")
 greg
 dev.off()
+getwd()
+shell.exec(paste0(getwd(), "/out/regression_depots.png"))
