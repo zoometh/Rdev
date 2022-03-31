c2257e1e4e6b99485a3ca10a20d9fa3d416c74be
zoometh <thomashuet7@gmail.com>
2022-03-31 20:06:13 +0100

    plotly

---
+++ time/damages.R
@@ -0,0 +1,8 @@
+library(plotly)
+library(htmlwidgets)
+library(openxlsx)
+
+df <- read.xlsx(paste0(getwd(),"/time/data.xlsx"))
+p <- plot_ly(df, type = 'scatter', x = ~date, y = ~hp, color = ~threat,
+             mode = 'line', stackgroup='one')
+saveWidget(as_widget(p), paste0(getwd(),"/time/threats.html"))
