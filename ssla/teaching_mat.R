28ba9c1f183a71a53d776af250141e473616ba57
zoometh <thomashuet7@gmail.com>
2022-04-24 09:06:11 +0100

    count tags

---
+++ ssla/teaching_mat.R
@@ -0,0 +1,36 @@
+library(googlesheets4)
+library(stringr)
+library(ggplot2)
+library(ggrepel)
+
+
+gsheet.base <- 'https://docs.google.com/spreadsheets/d/'
+gsheet.url <- '1oiOaKQu5sqj0IV99PY7Qr1FpvQ0QaEqH1pMKK9uDALw/edit#gid=0'
+gsheet <- paste0(gsheet.base, gsheet.url)
+xl <- read_sheet(gsheet)
+xl.tags.list <- str_split(xl$tags, ",")
+tags <- str_to_title(trimws(unlist(xl.tags.list)))
+df.tags <- data.frame(table(tags))
+df.tags <- df.tags[order(df.tags$Freq, decreasing = TRUE),]
+# rownames(df.tags) <- 1:nrow(df.tags)
+# rownames(df.tags) <- df.tags$tags
+# df.tags$tags <- NULL
+jpeg(paste0(getwd(), '/tags_occur.jpg'), width = 25, height = 17,
+     res = 300, unit='cm')
+ggplot(df.tags, aes(x = tags, y = Freq)) +
+  geom_bar(aes(fill = tags), stat = "identity", position ="dodge") +
+  geom_text_repel(aes(label = tags), max.overlaps = 40, size = 3) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(size = 7),
+        legend.key.size = unit(.7, "line"))
+dev.off()
+
+
+barplot(prop.table(table(xl.tags)))
+
+# counts
+ +
+  geom_text(aes(y = Freq, label=tags), vjust=0)
