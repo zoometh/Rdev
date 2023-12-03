68d8d57dd967ea4f585c6bb67b67bb564550a431
zoometh <thomashuet7@gmail.com>
2023-12-03 14:10:41 +0000

    wordcloud

--- text/soa_cloudword_1.R
+++ text/soa_cloudword_1.R
@@ -4,7 +4,7 @@ library(tm)
 library(tibble)
 
 
-research.soa <- function(soa.url = NA, soa.research.areas = NA, soa.stopwords = NA){
+research.soa <- function(soa.url = NA, soa.research.areas = NA, soa.stopwords = c("oxford", "school", "archaeolog", "research", "study", "seminar", "discussion", "projects", "periods", "forum", "collaborations", "well", "provid", "listed", "many", "including")){
   for(research.area in soa.research.areas){
     # research.area <- 'historical-and-classical'
     url <- paste0(soa.url, research.area)
@@ -12,9 +12,7 @@ research.soa <- function(soa.url = NA, soa.research.areas = NA, soa.stopwords = 
     paragraphs <- html_nodes(html_content, "p")
     website_text <- html_text(paragraphs, trim = TRUE)
     corp <- Corpus(VectorSource(website_text))
-
     corp <- tm_map(corp, stemDocument)
-
     corp <- tm_map(corp, removePunctuation)
     corp <- tm_map(corp, content_transformer(tolower))
     corp <- tm_map(corp, removeWords, stopwords("english"))
@@ -29,15 +27,16 @@ research.soa <- function(soa.url = NA, soa.research.areas = NA, soa.stopwords = 
     png(outWC, width = 900, height = 900, res = 150)
     wordcloud(words = df$word, freq = df$freq,
               min.freq = 1, max.words = 50,
-              random.order = FALSE, rot.per = 0.35,
+              random.order = FALSE, rot.per = 0.2,
               colors=brewer.pal(8, "Dark2"))
     dev.off()
     print(paste("Exported:", research.area))
   }
 }
 
-soa.url <- 'https://www.arch.ox.ac.uk/'
-soa.research.areas <- list('bioarchaeology', 'chronology', 'eurasian-prehistory', 'historical-and-classical', 'materials-and-technology', 'palaeolithic')
-soa.stopwords <- c("oxford", "school", "archaeolog", "research", "study", "seminar", "discussion", "projects", "periods", "forum", "collaborations", "well", "provid", "listed", "many", "including") # "archaeological",
-research.soa(soa.url, soa.research.areas, soa.stopwords)
+# devtools::source_url("https://raw.githubusercontent.com/zoometh/Rdev/master/text/soa_cloudword_1.R")
+#
+# soa.url <- 'https://www.arch.ox.ac.uk/'
+# soa.research.areas <- list('bioarchaeology', 'chronology', 'eurasian-prehistory', 'historical-and-classical', 'materials-and-technology', 'palaeolithic')
+# research.soa(soa.url, soa.research.areas)
 
